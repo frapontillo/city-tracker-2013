3196204351054a5f3d2179f7a1b2948f1a028ef5
Francesco Ditrani <f.ditrani@gmail.com>
2013-05-23 18:00:53 +0200

    Selezione foto

--- src/it/auh/citytracker/MainActivity.java
+++ src/it/auh/citytracker/MainActivity.java
@@ -2,6 +2,7 @@ package it.auh.citytracker;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +11,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, SenderActivity.class));
     }
 
 
@@ -19,5 +21,7 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    
+    
     
 }
